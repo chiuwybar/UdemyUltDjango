ab27c36719179f4316074bf6a5e93fd431a55099
chiuwybar <leafhockey13@gmail.com>
2019-02-12 12:22:36 -0500

    translation

--- piglatin/piglatin/views.py
+++ piglatin/piglatin/views.py
@@ -6,4 +6,22 @@ def home (request):
 
 def translate(request):
 	
-	return HttpResponse("You're on the translate page! " + request.GET['originaltext'])
+	original = request.GET['originaltext'].lower()
+
+	translation = ' '
+
+	for word in original.split():
+		if word[0] in ['a', 'e', 'i', 'o', 'u']:
+		# vowel
+		translation += word
+		translation += 'yay '
+		else:
+		#  consonant
+		# heese
+		# c
+		translation ++ word[1:]
+		# ay
+		translation += word[0]
+		translation += 'ay '
+
+	return HttpResponse(translation)
