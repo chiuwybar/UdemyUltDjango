762eef0a7edfe931a21169f8876bf09373f490ff
kmanchu <kmanchu@MacBook-Pro.local>
2019-02-18 11:13:34 -0500

    added translate.html

--- piglatin/piglatin/urls.py
+++ piglatin/piglatin/urls.py
@@ -19,7 +19,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
     path('translate/', views.translate, name="translate"),
 
 ]
--- piglatin/piglatin/views.py
+++ piglatin/piglatin/views.py
@@ -13,15 +13,15 @@ def translate(request):
 	for word in original.split():
 		if word[0] in ['a', 'e', 'i', 'o', 'u']:
 		# vowel
-		translation += word
-		translation += 'yay '
+			translation += word
+			translation += 'yay '
 		else:
 		#  consonant
 		# heese
 		# c
-		translation ++ word[1:]
+			translation += word[1:]
 		# ay
-		translation += word[0]
-		translation += 'ay '
+			translation += word[0]
+			translation += 'ay '
 
-	return HttpResponse(translation)
+	return render(request, 'translate.html', {'original':original, 'translation':translation}) 
